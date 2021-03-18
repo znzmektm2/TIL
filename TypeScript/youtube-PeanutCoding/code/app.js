f83ed8d94f6a3219e9e9216ace91587d2805cbe6
znzmektm2 <tingcber2@naver.com>
2021-03-19 00:16:54 +0900

    타입스크립트 인트로

---
+++ TypeScript/youtube-PeanutCoding/code/app.js
@@ -0,0 +1,6 @@
+// function logName(name: string) {
+//     console.log(name);
+// }
+// logName('Jack');
+// console.log('Hello');
+var age = 10;
---
+++ TypeScript/youtube-PeanutCoding/code/app.ts
@@ -0,0 +1,9 @@
+// function logName(name: string) {
+//     console.log(name);
+// }
+
+// logName('Jack');
+
+// console.log('Hello');
+
+let age: number = 10;
