aa5ae95708b19ff347449e02b4e51db36f1c22ba
znzmektm2 <tingcber2@naver.com>
2021-03-19 19:43:31 +0900

    인터페이스

--- TypeScript/code/app.js
+++ TypeScript/code/app.js
@@ -1,11 +1,33 @@
-// //*** 설치와 환경 설정
+// //** 설치와 환경 설정 */
 // function logName(name: string) {
 //     console.log(name);
 // }
 // logName('Jack');
 // console.log('Hello');
 // let age: number = 10;
-// //*** 타입 추론, 타입 명시
-function calculator(points) {
-    return 100 - points;
+// //** 타입 추론, 타입 명시 */
+// function calculator(points) {
+//     return 100 - points;
+// }
+// //** 인터페이스 */
+var student1 = {
+    studentID: 12345,
+    studentName: "Jake",
+    age: 20,
+    gender: 'male',
+    subject: "TypeScript",
+    courseCompleted: true
+};
+// function getStudentDetails (studentID: number): Student {
+//     return {
+//         studentID: 12345,
+//         studentName: "Jake",
+//         gender: 'male',
+//         subject: "TypeScript",
+//         courseCompleted: true
+//     }
+// }
+function saveStudentDetails(student) {
+    //student.studentID = 1111;
 }
+saveStudentDetails(student1);
--- TypeScript/code/app.ts
+++ TypeScript/code/app.ts
@@ -1,4 +1,4 @@
-// //*** 설치와 환경 설정
+// //** 설치와 환경 설정 */
 
 // function logName(name: string) {
 //     console.log(name);
@@ -12,8 +12,46 @@
 
 
 
-// //*** 타입 추론, 타입 명시
+// //** 타입 추론, 타입 명시 */
 
-function calculator(points) {
-    return 100 - points;
+// function calculator(points) {
+//     return 100 - points;
+// }
+
+
+// //** 인터페이스 */
+let student1 = {
+    studentID: 12345,
+    studentName: "Jake",
+    age: 20,
+    gender: 'male',
+    subject: "TypeScript",
+    courseCompleted: true
 }
+
+interface Student {
+    readonly studentID: number;
+    studentName: string;
+    age?: number;
+    gender: string;
+    subject: string;
+    courseCompleted: boolean;
+    // addComment (comment: string): string;
+    addComment?: (comment: string) => string;
+}
+
+// function getStudentDetails (studentID: number): Student {
+//     return {
+//         studentID: 12345,
+//         studentName: "Jake",
+//         gender: 'male',
+//         subject: "TypeScript",
+//         courseCompleted: true
+//     }
+// }
+
+function saveStudentDetails(student: Student): void {
+    //student.studentID = 1111;
+}
+
+saveStudentDetails(student1);
